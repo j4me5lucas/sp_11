6ad56377dc0bdf22f8eecd5013b1f666e37413ec
Administrator <Administrator@USER-20161013UJ>
2019-06-28 10:25:34 +0800

    1111

---
+++ sp12-codetest-12/src/main/java/com/tedu/sp12/Sp12Codetest12Application.java
@@ -0,0 +1,13 @@
+package com.tedu.sp12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp12Codetest12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp12Codetest12Application.class, args);
+	}
+
+}
